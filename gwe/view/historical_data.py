70ed77cbe5e2a183fb133a0c5c82d51fb53f8269
Roberto Leinardi <leinardi@gmail.com>
2019-01-09 21:52:11 +0100

    Set Replaced gsi with gwe

--- gwe/view/historical_data.py
+++ gwe/view/historical_data.py
@@ -2,18 +2,18 @@
 #
 # Copyright (c) 2018 Roberto Leinardi
 #
-# gsi is free software: you can redistribute it and/or modify
+# gwe is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# gsi is distributed in the hope that it will be useful,
+# gwe is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with gsi.  If not, see <http://www.gnu.org/licenses/>.
+# along with gwe.  If not, see <http://www.gnu.org/licenses/>.
 import time
 import logging
 from typing import Dict, Tuple, Any
