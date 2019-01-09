24fb2a8dcfe5f91c2afa8e9eace3756efeebd13c
Roberto Leinardi <leinardi@gmail.com>
2019-01-09 22:07:52 +0100

    Fixed repo name

--- scripts/build_flatpak.py
+++ scripts/build_flatpak.py
@@ -9,7 +9,7 @@ manifest = sys.argv[1]
 output = sys.argv[2]
 app_id = path.basename(manifest).rpartition('.')[0]
 
-with TemporaryDirectory(prefix='hydrapaper-flatpak-repo') as temprepo:
-    with TemporaryDirectory(prefix='hydrapaper-flatpak-build') as tempbuild:
+with TemporaryDirectory(prefix='gwe-flatpak-repo') as temprepo:
+    with TemporaryDirectory(prefix='gwe-flatpak-build') as tempbuild:
         subprocess.call(['flatpak-builder', tempbuild, manifest, '--repo=' + temprepo])
     subprocess.call(['flatpak', 'build-bundle', temprepo, output, app_id])
