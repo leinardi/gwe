d25c5328a7eada5f0fb37b70706a970ba7c68462
Roberto Leinardi <leinardi@gmail.com>
2019-01-09 23:33:32 +0100

    Updated misc flatpak manifest

--- scripts/build_flatpak.py
+++ scripts/build_flatpak.py
@@ -11,5 +11,5 @@ app_id = path.basename(manifest).rpartition('.')[0]
 
 with TemporaryDirectory(prefix='gwe-flatpak-repo') as temprepo:
     with TemporaryDirectory(prefix='gwe-flatpak-build') as tempbuild:
-        subprocess.call(['flatpak-builder', tempbuild, manifest, '--repo=' + temprepo])
+        subprocess.call(['flatpak-builder', ' --install-deps-from=flathub', tempbuild, manifest, '--repo=' + temprepo])
     subprocess.call(['flatpak', 'build-bundle', temprepo, output, app_id])
