e7bea2165a5ecb73275116a975a219af9e9a01bf
Roberto Leinardi <leinardi@gmail.com>
2019-01-11 22:41:04 +0100

    Added ability to build flatpak from local or remote source

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ MESON_BUILD_DIR="build/meson"
 OUTPUT_DIR="build/output"
 INSTALL_DIR="${OUTPUT_DIR}/install"
 
-[[ -d ${MESON_BUILD_DIR} ]] && rm -rfv ${MESON_BUILD_DIR}
+[[ -d ${OUTPUT_DIR} ]] && rm -rfv ${OUTPUT_DIR}
+# keep .flatpak-builder
+find ${MESON_BUILD_DIR}/* ${MESON_BUILD_DIR}/.[^.]* -maxdepth 0 -not -name '.flatpak-builder' -exec rm -rv {} +
 find . -regex '^.*\(__pycache__\|\.py[co]\)$' -delete
 mkdir -pv ${MESON_BUILD_DIR} ${INSTALL_DIR}
 meson . ${MESON_BUILD_DIR} --prefix=$PWD/${INSTALL_DIR} $@
--- scripts/build_flatpak.py
+++ scripts/build_flatpak.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 flatpak_build_path = Path(__file__).parent.parent / 'build' / 'flatpak'
 manifest = sys.argv[1]
-output = sys.argv[2]
+output_file = flatpak_build_path.parent / 'output' / sys.argv[2]
 app_id = path.basename(manifest).rpartition('.')[0]
 
 repo_path = flatpak_build_path / 'repo'
@@ -15,6 +15,7 @@ build_path = flatpak_build_path / 'build'
 
 repo_path.mkdir(parents=True, exist_ok=True)
 build_path.mkdir(parents=True, exist_ok=True)
+output_file.parent.mkdir(parents=True, exist_ok=True)
 
 print(str(build_path))
 print(str(manifest))
@@ -27,4 +28,4 @@ subprocess.call(
      str(build_path), str(manifest),
      '--repo=' + str(repo_path)
      ])
-subprocess.call(['flatpak', 'build-bundle', str(repo_path), output, app_id])
+subprocess.call(['flatpak', 'build-bundle', str(repo_path), str(output_file), app_id])
