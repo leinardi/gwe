51c1742b22e47f051d2a12fbd53325c24c2e9456
Roberto Leinardi <leinardi@gmail.com>
2021-08-28 12:58:07 +0200

    Prepare for release 0.15.4

--- gwe/conf.py
+++ gwe/conf.py
@@ -19,7 +19,7 @@ from typing import Dict, Any
 APP_PACKAGE_NAME = "gwe"
 APP_NAME = "GWE"
 APP_ID = "com.leinardi.gwe"
-APP_VERSION = "0.15.3"
+APP_VERSION = "0.15.4"
 APP_ICON_NAME = APP_ID
 APP_ICON_NAME_SYMBOLIC = APP_ID + "-symbolic"
 APP_DB_NAME = APP_PACKAGE_NAME + ".db"
