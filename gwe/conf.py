3b6bcc5911da1cfed54cfb7af562163021eea0b5
Ariche2 <leo.burbridge1@gmail.com>
2024-05-29 15:19:56 +0000

    Update version number

--- gwe/conf.py
+++ gwe/conf.py
@@ -19,7 +19,7 @@ from typing import Dict, Any
 APP_PACKAGE_NAME = "gwe"
 APP_NAME = "GWE"
 APP_ID = "com.leinardi.gwe"
-APP_VERSION = "0.15.7"
+APP_VERSION = "0.15.8"
 APP_ICON_NAME = APP_ID
 APP_ICON_NAME_SYMBOLIC = APP_ID + "-symbolic"
 APP_DB_NAME = APP_PACKAGE_NAME + ".db"
