4ed52b1957510062614d8b8052a975b8da9dc3ae
Kai Schulz <winblocker@gmx.de>
2024-07-06 15:58:37 +0200

    Fix license typos

--- gwe/interactor/set_fan_speed_interactor.py
+++ gwe/interactor/set_fan_speed_interactor.py
@@ -2,7 +2,7 @@
 #
 # Copyright (c) 2020 Roberto Leinardi
 #
-# gst is free software: you can redistribute it and/or modify
+# gwe is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
